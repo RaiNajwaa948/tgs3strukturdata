a7b7b35d606094848332b51b3d9dc80305d3fa7d
RaiNajwaa948 <c050423019@mahasiswa.poliban.ac.id>
2024-12-13 14:37:02 +0800

    u:melakukan sorting asc

--- menu.cpp
+++ menu.cpp
@@ -16,7 +16,7 @@ int main()
   char pl;
   cout << "Masukkan jumlah data (maksimal 100): ";
   cin >> n; // Mengatur jumlah data di sini
-  if (n > 100) n = 100; // Batasi jumlah data maksimal 100
+  if (n > 100) n = 5; 
 
   do
   {
@@ -91,4 +91,20 @@ void tukar(int *a,int *b){
 int t=*a;
 *a=*b;
 *b=t;
+}
+
+void sortingAsc()
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arrdata[j] > arrdata[j + 1])
+            {
+                tukar(&arrdata[j], &arrdata[j + 1]);
+            }
+        }
+    }
+    cout << "Data berhasil diurutkan secara ascending.\n";
+    getch();
 }
